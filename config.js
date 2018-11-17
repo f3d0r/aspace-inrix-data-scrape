08bd98d396e9e20eb2b17f1e3082b51e0dcea08f
Fedor Paretsky <me@f3d0r.com>
2018-11-16 22:02:08 -0800

    Remove deprecated files

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    FILES: {
-        LOCAL_PROXY_FILE_NAME: "exported-proxies.txt",
-        LOCAL_INRIX_ID_FILE_NAME: "parking_id_test.txt",
-    },
-    ACCESS_TOKENS: {
-        MAPBOX: '***REMOVED***'
-    }
-};
---
+++ config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    FILES: {
+        LOCAL_INRIX_ID_FILE_NAME: "parking_ids.txt"
+    },
+    ACCESS_TOKENS: {
+        MAPBOX: '***REMOVED***'
+    }
+};
